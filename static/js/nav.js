2d73445f1ecd94c1d0a3def3e7b156f605868058
greg <greg.goforth@gmail.com>
2014-06-24 00:48:23 -0700

    ajax nav, styling all around

---
+++ static/js/nav.js
@@ -0,0 +1,10 @@
+/*global $*/
+(function () {
+
+    "use strict";
+
+    //load the navigation into the site so I don't have to update a million pages
+    //just to update some links
+    $("header#nav").load("nav.html");
+
+}());
